062840efe92f4cd994b18cf3320ad2ad2bceaf77
SharlyeN <carlos.navarro@3mkt.com.mx>
2023-10-28 10:54:19 -0600

    9th commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,24 @@ def hello():
 def mostrar_imagen():
     return render_template('index.html')
 
+
+
+# Ruta para recibir los datos de Shopify a través de un webhook
+@app.route('/shopify-webhook', methods=['POST'])
+def shopify_webhook():
+    # Verifica que el contenido sea JSON
+    if request.is_json:
+        data = request.get_json()
+        # Procesa los datos JSON recibidos
+
+        # Aquí puedes agregar tu lógica para manejar los datos de Shopify
+
+        # Devuelve una respuesta (opcional)
+        response_data = {"message": "Datos de Shopify recibidos correctamente"}
+        return jsonify(response_data), 200
+    else:
+        return "El contenido no es JSON", 400
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
-
