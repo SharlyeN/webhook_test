34519dd88a4bdd541e93b774bf24b7e6f03f60a4
SharlyeN <carlos.navarro@3mkt.com.mx>
2023-10-28 10:41:54 -0600

    9th commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola mundo desde NGINX y AWS"
+    return "Hola mundo desde NGINX y AWS pero con mas texto"
+
+@app.route('/home')
+def mostrar_imagen():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
